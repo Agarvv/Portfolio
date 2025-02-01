c4fec771d5721e2c431773ce2a6f9dcae2263c9d
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-02-01 19:46:26 +0000

    homepage

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './components/layouts/main-layout/main-layout.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: '',
+      component: MainLayoutComponent,
+      children: [
+        {
+            path: '',
+            component: HomepageComponent
+        }
+      ]
+    }
+];
