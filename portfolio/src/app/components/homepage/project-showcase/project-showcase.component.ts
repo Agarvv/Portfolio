a619f70079d5dbc3ba04a4c6eb6109f13193093e
Agarvv <casluagarv@gmail.com>
2025-02-02 16:47:15 +0100

    Angular issues

--- portfolio/src/app/components/homepage/project-showcase/project-showcase.component.ts
+++ portfolio/src/app/components/homepage/project-showcase/project-showcase.component.ts
@@ -1,16 +1,17 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-project-showcase',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], 
   templateUrl: './project-showcase.component.html',
   styleUrls: ['./project-showcase.component.css']
 })
 export class ProjectShowcaseComponent {
-   @Input() name: string; 
-   @Input() image: string; 
-   @Input() description: string; 
-   @Input() techStack: string[];  
-   @Input() functionalities: string[]; 
+   @Input() name!: string; 
+   @Input() image!: string;
+   @Input() description!: string;
+   @Input() techStack!: string[];
+   @Input() functionalities!: string[];
 }
