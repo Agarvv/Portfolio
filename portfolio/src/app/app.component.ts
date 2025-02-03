707452008f02340e485ea41f19da317c466070c7
Agarvv <casluagarv@gmail.com>
2025-02-03 21:50:42 +0100

    Translation p2

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'portfolio';
+  constructor(private translate: TranslateService) {
+    
+    this.translate.setDefaultLang('fr');
+    this.translate.use('fr');
+  }
 }
