eb5652e45e1a629610961526bb334f5d94f5fba4
Agarvv <casluagarv@gmail.com>
2025-02-03 23:11:58 +0100

    issues

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
-//import { TranslateService } from '@ngx-translate/core'; 
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'app-root',
+imports: [RouterOutlet],
+templateUrl: './app.component.html',
+styleUrl: './app.component.css'
 })
 export class AppComponent {
-  /*constructor(private translate: TranslateService) {
-    
-    this.translate.setDefaultLang('fr');
-    this.translate.use('fr');
-  }*/ 
+
 }
