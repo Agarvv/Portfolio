9ae4fd9b429d38364b616d4d4a64c8dd6eaf65c0
Agarvv <casluagarv@gmail.com>
2025-02-04 00:16:58 +0100

    issues

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-selector: 'app-root',
-imports: [RouterOutlet],
-templateUrl: './app.component.html',
-styleUrl: './app.component.css'
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+    HttpClientModule,
+  ],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('fr');
+    this.translate.use('fr'); 
+  }
 
+  
+}
+
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
