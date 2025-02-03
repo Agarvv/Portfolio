5661b931115a3cbe7e4ebc0415b5b86bcf021ffb
Agarvv <casluagarv@gmail.com>
2025-02-03 21:55:24 +0100

    issues

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common'; 
-import { AppComponent } from './app.component'; 
-import { HttpClientModule } from '@angular/common/http'; 
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core'; 
-import { TranslateHttpLoader } from '@ngx-translate/http-loader'; 
-
+import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http'; 
+import { RouterModule } from '@angular/router'; 
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -13,21 +13,22 @@ export function HttpLoaderFactory(http: HttpClient) {
 
 @NgModule({
   declarations: [
-    AppComponent, 
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    CommonModule, 
+    CommonModule,
     HttpClientModule,
+    RouterModule.forRoot([]),  
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
         useFactory: HttpLoaderFactory,
         deps: [HttpClient]
       }
-    }), 
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent] 
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
