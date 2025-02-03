047385211789fbf95ac40e0dbc55b626cf36cb31
Agarvv <casluagarv@gmail.com>
2025-02-03 21:41:32 +0100

    Translations

---
+++ portfolio/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common'; 
+import { AppComponent } from './app.component'; 
+import { HttpClientModule } from '@angular/common/http'; 
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core'; 
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'; 
+
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+@NgModule({
+  declarations: [
+    AppComponent, 
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule, 
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }), 
+  ],
+  providers: [],
+  bootstrap: [AppComponent] 
+})
+export class AppModule { }
