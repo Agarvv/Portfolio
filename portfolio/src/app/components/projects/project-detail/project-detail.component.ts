a11918930cea37bd269a73ebf414a655cf59bc85
Agarvv <casluagarv@gmail.com>
2025-02-03 18:23:53 +0100

    issues

--- portfolio/src/app/components/projects/project-detail/project-detail.component.ts
+++ portfolio/src/app/components/projects/project-detail/project-detail.component.ts
@@ -11,6 +11,6 @@ export class ProjectDetailComponent {
   @Input() detail: string = 'Project Title';  
   @Input() description: string = 'Project Description';  
   @Input() detailMediaType: string = 'img';  
-  @Input() detailMedia: string = 'assets/Showroom_508_M_Allure_1.jpg';  
-  @Input() responsiveDetailMedia: string | null = 'assets/Showroom_508_M_Allure_1.jpg';  
+  @Input() detailMedia: string = 'https://images.unsplash.com/photo-1516259762381-22954d7d3ad2?q=80&w=1489&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';  
+  @Input() responsiveDetailMedia: string | null = 'https://images.unsplash.com/photo-1516259762381-22954d7d3ad2?q=80&w=1489&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';  
 }
