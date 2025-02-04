35edc5a9d691e355b38fec14fe5a69b6bc18b7b4
Agarvv <casluagarv@gmail.com>
2025-02-04 01:31:29 +0100

    issues

--- portfolio/src/main.ts
+++ portfolio/src/main.ts
@@ -1,6 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { TranslateModule } from '@ngx-translate/core';  
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {
+  providers: [
+    ...appConfig.providers,
+    
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+  ],
+})
   .catch((err) => console.error(err));
