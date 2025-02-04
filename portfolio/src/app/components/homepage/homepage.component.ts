a52d0bbd04b360c5af6d609d1fcba3e47b42f33a
Agarvv <casluagarv@gmail.com>
2025-02-04 18:30:29 +0100

    more and more and more and more issues

--- portfolio/src/app/components/homepage/homepage.component.ts
+++ portfolio/src/app/components/homepage/homepage.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 import { ProjectShowcaseComponent } from './project-showcase/project-showcase.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [ProjectShowcaseComponent, TranslateModule],  
+  imports: [ProjectShowcaseComponent, TranslateModule, CommonModule],
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
+  functionalitiesProject1: string[] = [];
+  functionalitiesProject2: string[] = [];
+  functionalitiesProject3: string[] = [];
 
+  constructor(public translate: TranslateService) {}
+
+  ngOnInit() {
+    this.loadTranslations();
+
+    this.translate.onLangChange.subscribe(() => {
+      this.loadTranslations();
+    });
+  }
+
+  loadTranslations() {
+    this.translate.get('projects').subscribe(projects => {
+      this.functionalitiesProject1 = projects?.project1?.functionalities || [];
+      this.functionalitiesProject2 = projects?.project2?.functionalities || [];
+      this.functionalitiesProject3 = projects?.project3?.functionalities || [];
+    });
+  }
 }
--- portfolio/src/app/components/homepage/project-showcase/project-showcase.component.ts
+++ portfolio/src/app/components/homepage/project-showcase/project-showcase.component.ts
@@ -5,30 +5,32 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-project-showcase',
   standalone: true,
-  imports: [CommonModule, TranslateModule], 
+  imports: [CommonModule],
   templateUrl: './project-showcase.component.html',
   styleUrls: ['./project-showcase.component.css']
 })
-export class ProjectShowcaseComponent implements OnInit {
+export class ProjectShowcaseComponent  {
 
-   @Input() name!: string; 
-   @Input() image!: string;
-   @Input() description!: string; 
-   @Input() techStack!: string[];
-   @Input() functionalities!: string[]; 
+  @Input() name!: string;
+  @Input() image!: string;
+  @Input() description!: string;
+  @Input() techStack!: string[];
+  
+  translatedDescription!: string;
+  @Input() functionalities!: string[];
+  @Input() translatedFunctionalities!: string[];
 
-   translatedDescription!: string;
-   translatedFunctionalities!: string[];
+ /* constructor(private translate: TranslateService) {}
 
-   constructor(private translate: TranslateService) {}
+  ngOnInit() {
+    this.translate.get(this.description).subscribe(desc => {
+      this.translatedDescription = desc;
+    });
 
-   ngOnInit() {
-      this.translate.get(this.description).subscribe(desc => {
-         this.translatedDescription = desc;
-      });
-
-      this.translate.get(this.functionalities).subscribe(funcs => {
-         this.translatedFunctionalities = funcs;
-      });
-   }
+    this.translate.get(this.functionalities).subscribe(funcs => {  
+      this.translatedFunctionalities = funcs;  
+    });
+  }
+  */
+  
 }
