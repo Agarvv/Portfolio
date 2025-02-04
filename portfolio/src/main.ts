f6ffef61fd7d4b39656a63adf2b454a95c3f2343
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-02-04 01:00:24 +0000

    issues

--- portfolio/src/main.ts
+++ portfolio/src/main.ts
@@ -1,8 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';  
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export function HttpLoaderFactory(http: HttpClient) {
@@ -11,22 +14,14 @@ export function HttpLoaderFactory(http: HttpClient) {
 
 bootstrapApplication(AppComponent, {
   providers: [
-    ...appConfig.providers, 
+    ...appConfig.providers,
+    provideHttpClient(),
+    provideRouter(routes),
     {
       provide: TranslateLoader,
       useFactory: HttpLoaderFactory,
       deps: [HttpClient],
     },
+    TranslateService, 
   ],
-  imports: [
-    HttpClientModule,  
-    TranslateModule.forRoot({  
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient],
-      },
-    }),
-  ],
-})
-  .catch((err) => console.error(err));
+}).catch((err) => console.error(err));
