894522745ae9957405530c66fbf04af3c368b648
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-02-04 01:04:39 +0000

    issues

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],  
+  imports: [RouterOutlet, TranslateModule],  
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
