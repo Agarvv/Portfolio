12e686aa5a067160daba5db11cfc2d52556f63ac
Agarvv <casluagarv@gmail.com>
2025-02-04 15:57:12 +0100

    issues while translating p showcase description

--- portfolio/src/app/components/homepage/project-showcase/project-showcase.component.ts
+++ portfolio/src/app/components/homepage/project-showcase/project-showcase.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-project-showcase',
@@ -9,11 +9,26 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './project-showcase.component.html',
   styleUrls: ['./project-showcase.component.css']
 })
-export class ProjectShowcaseComponent {
+export class ProjectShowcaseComponent implements OnInit {
 
    @Input() name!: string; 
    @Input() image!: string;
-   @Input() description!: string;
+   @Input() description!: string; 
    @Input() techStack!: string[];
-   @Input() functionalities!: string[];
+   @Input() functionalities!: string[]; 
+
+   translatedDescription!: string;
+   translatedFunctionalities!: string[];
+
+   constructor(private translate: TranslateService) {}
+
+   ngOnInit() {
+      this.translate.get(this.description).subscribe(desc => {
+         this.translatedDescription = desc;
+      });
+
+      this.translate.get(this.functionalities).subscribe(funcs => {
+         this.translatedFunctionalities = funcs;
+      });
+   }
 }
