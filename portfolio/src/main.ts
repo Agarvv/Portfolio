169bab82f1f5fe70065ebcf0d859286fc065b5e5
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-02-04 01:13:07 +0000

    issues

--- portfolio/src/main.ts
+++ portfolio/src/main.ts
@@ -4,24 +4,13 @@ import { appConfig } from './app/app.config';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
-import { HttpClient } from '@angular/common/http';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
+import { provideTranslations } from '@ngx-translate/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
     ...appConfig.providers,
     provideHttpClient(),
     provideRouter(routes),
-    {
-      provide: TranslateLoader,
-      useFactory: HttpLoaderFactory,
-      deps: [HttpClient],
-    },
-    TranslateService, 
+    provideTranslations(),  
   ],
 }).catch((err) => console.error(err));
