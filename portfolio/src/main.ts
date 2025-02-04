2a079991c9bccd40ef15f77177d236c201729944
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-02-04 01:02:11 +0000

    issues

--- portfolio/src/main.ts
+++ portfolio/src/main.ts
@@ -3,7 +3,7 @@ import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
+import { routes } from './app/app.routes';
 import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
 import { HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
