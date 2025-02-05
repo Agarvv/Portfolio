fc057f9807d2ef8a9a624fc3d4bc3dbe12b5a935
Agarvv <casluagarv@gmail.com>
2025-02-05 02:08:25 +0100

    issues'

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './components/layouts/main-layout/main-layout.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { SofiiProjectDetailsComponent } from './components/projects/sofii-project-details/sofii-project-details.component' 
+import { JagarvProjectDetailsComponent } from './components/projects/jagarv-project-details/jagarv-project-details.component'
 
 export const routes: Routes = [
     {
@@ -15,6 +16,10 @@ export const routes: Routes = [
         {
             path: 'sofii',
             component: SofiiProjectDetailsComponent
+        },
+        {
+            path: 'jagarv',
+            component: JagarvProjectDetailsComponent
         }
       ]
     }
--- portfolio/src/app/components/projects/jagarv-project-details/jagarv-project-details.component.ts
+++ portfolio/src/app/components/projects/jagarv-project-details/jagarv-project-details.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 
+import { ProjectDetailComponent } from '../project-detail/project-detail.component'
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
+
+
 @Component({
   selector: 'app-jagarv-project-details',
   standalone: true,
-  imports: [],
+  imports: [ProjectDetailComponent, TranslateModule, CommonModule]
   templateUrl: './jagarv-project-details.component.html',
   styleUrl: './jagarv-project-details.component.css'
 })
 export class JagarvProjectDetailsComponent {
-
+  constructor(public translate: TranslateService) {}
 }
