dc91209246ebd4ed3fa24267b82269a6cbe20d91
Agarvv <casluagarv@gmail.com>
2025-02-06 17:26:20 +0100

    issues

--- portfolio/src/app/components/projects/project-detail/project-detail.component.ts
+++ portfolio/src/app/components/projects/project-detail/project-detail.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-project-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './project-detail.component.html',
   styleUrls: ['./project-detail.component.css']
 })
