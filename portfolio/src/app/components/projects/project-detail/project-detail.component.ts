a0c4860da148050609e9b091ae32c824b22ae1a9
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-02-06 13:56:17 +0000

    issues

--- portfolio/src/app/components/projects/project-detail/project-detail.component.ts
+++ portfolio/src/app/components/projects/project-detail/project-detail.component.ts
@@ -11,7 +11,7 @@ import { Component, Input } from '@angular/core';
 export class ProjectDetailComponent {
   @Input() detail: string = 'Project Title';  
   @Input() description: string = 'Project Description';  
-  @Input() detailMediaType: string = 'img';  
-  @Input() detailMedia: string = 'https://images.unsplash.com/photo-1516259762381-22954d7d3ad2?q=80&w=1489&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';  
-  @Input() responsiveDetailMedia: string | null = 'https://images.unsplash.com/photo-1516259762381-22954d7d3ad2?q=80&w=1489&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';  
+  @Input() detailMediaType: string = '';  
+  @Input() detailMedia: string = '';  
+  @Input() responsiveDetailMedia: string | null = null; 
 }
