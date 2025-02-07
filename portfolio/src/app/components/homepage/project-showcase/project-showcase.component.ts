4de7f6f885d9ccf00dd7f4ea1bd6bbae96c96357
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-02-07 00:36:05 +0000

    routings

--- portfolio/src/app/components/homepage/project-showcase/project-showcase.component.ts
+++ portfolio/src/app/components/homepage/project-showcase/project-showcase.component.ts
@@ -2,11 +2,12 @@ import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { GithubBtnComponent } from './github-btn/github-btn.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-project-showcase',
   standalone: true,
-  imports: [GithubBtnComponent, CommonModule, TranslateModule],
+  imports: [GithubBtnComponent, CommonModule, TranslateModule, RouterModule],
   templateUrl: './project-showcase.component.html',
   styleUrls: ['./project-showcase.component.css']
 })
