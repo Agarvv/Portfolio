4ea6d2b35e7f734c8cd4a72e683a651b469c3c74
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-02-07 00:01:11 +0000

    github button

---
+++ portfolio/src/app/components/homepage/project-showcase/github-btn/github-btn.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-github-btn',
+  imports: [],
+  templateUrl: './github-btn.component.html',
+  styleUrl: './github-btn.component.css'
+})
+export class GithubBtnComponent {
+
+}
--- portfolio/src/app/components/homepage/project-showcase/project-showcase.component.ts
+++ portfolio/src/app/components/homepage/project-showcase/project-showcase.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { GithubBtnComponent } from './github-btn/github-btn.component';
 
 @Component({
   selector: 'app-project-showcase',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [GithubBtnComponent, CommonModule, TranslateModule],
   templateUrl: './project-showcase.component.html',
   styleUrls: ['./project-showcase.component.css']
 })
